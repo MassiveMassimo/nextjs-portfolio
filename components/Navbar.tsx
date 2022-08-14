7124fa1693a3ea7065174b12408819b4d426f2cc
MassiveMassimo <muhammad.hannan@ui.ac.id>
2022-08-14 23:17:26 +0700

    Fixed null pointer error

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,9 +39,9 @@ if (typeof window !== "undefined") {
     window.onscroll = function () {
         let currentScrollPos = window.pageYOffset;
         if (prevScrollpos > currentScrollPos) {
-            document.getElementById("navbar").style.top = "0";
+            document.getElementById("navbar")!.style.top = "0";
         } else {
-            document.getElementById("navbar").style.top = "-72px";
+            document.getElementById("navbar")!.style.top = "-72px";
         }
         prevScrollpos = currentScrollPos;
     }
