4facc66daf7faf354e55959464a2c90a7d72b2a0
MassiveMassimo <muhammad.hannan@ui.ac.id>
2022-10-08 19:33:42 +0700

    Added typing animation to hero and more about me

--- components/Landing/Responsive.tsx
+++ components/Landing/Responsive.tsx
@@ -1,70 +1,159 @@
 import styles from "../../styles/responsive.module.scss";
+import Image from "next/image";
 
 export default function Responsive() {
+  const container =
+    typeof window !== "undefined"
+      ? document.getElementById("responsive")
+      : null;
+  const handle =
+    typeof window !== "undefined" ? document.getElementById("handle") : null;
 
-    const container = typeof window !== 'undefined' ? document.getElementById('responsive') : null;
-    const handle = typeof window !== 'undefined' ? document.getElementById('handle') : null;
+  if (handle) {
+    handle.addEventListener("mousedown", rightMouseDown);
+  }
 
-    if (handle) {
-        handle.addEventListener('mousedown', rightMouseDown);
+  function rightMouseDown(e: { preventDefault: () => void }) {
+    e.preventDefault();
+    document.onmousemove = rightElementDrag;
+    document.onmouseup = closeDragElement;
+  }
+
+  function rightElementDrag(e: MouseEvent) {
+    e = e || window.event;
+    e.preventDefault();
+
+    let width = 0;
+    if (container != null) {
+      width = e.clientX - container.offsetLeft;
     }
 
-    function rightMouseDown(e: { preventDefault: () => void; }) {
-        e.preventDefault();
-        document.onmousemove = rightElementDrag;
-        document.onmouseup = closeDragElement;
+    const parentWidth =
+      typeof window !== "undefined"
+        ? document.getElementById("responsiveParent")?.offsetWidth
+        : 0;
+
+    if (parentWidth != undefined) {
+      if (container != null && width >= 450 && width <= parentWidth + 64) {
+        container.style.width = `${width - 72}px`;
+
+        // console.log("parentWidth: " + parentWidth);
+        // console.log("e.clientX: " + e.clientX);
+        // console.log("width: " + width);
+      }
     }
+  }
 
-    function rightElementDrag(e: MouseEvent) {
-        e = e || window.event;
-        e.preventDefault();
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 
-        let width = 0;
-        if (container != null) {
-            width = e.clientX - container.offsetLeft;
-        }
-
-        const pwidth = typeof window !== 'undefined' ? document.getElementById('responsiveParent')?.offsetWidth : 0;
-
-        if (pwidth != undefined) {
-            if (container != null && width >= 450 && width <= pwidth+64) {
-                container.style.width = `${width - 64}px`;
-
-                // console.log("pwidth: " + pwidth);
-                // console.log("e.clientX: " + e.clientX);
-                // console.log("width: " + width);
-            }
-        }
-    }
-
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-
-    return (
-        <div id="responsive" style={{ position: "relative", width: "100%", height: "100%", backgroundColor: "#FFFFFF" }}>
-            <div id="handle" style={{ display: "flex", position: "absolute", width: "0.5rem", height: "100%", borderRadius: "0.25rem", top: 0, right: 0, alignItems: "center", backgroundColor: "#8A8A8A", cursor: "col-resize" }}>
-                <div style={{ display: "flex", position: "absolute", left: "-0.75rem", width: "2rem", height: "2rem", borderRadius: "1rem", backgroundColor: "#ccc", alignItems: "center" }}>
-                    <svg style={{ margin: "auto" }} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fillRule="evenodd" clipRule="evenodd" d="M12 4C12.5523 4 13 4.44772 13 5L13 19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19L11 5C11 4.44772 11.4477 4 12 4Z" fill="black" fillOpacity="0.4" />
-                        <path fillRule="evenodd" clipRule="evenodd" d="M15 10.9998C14.4477 10.9998 14 11.4475 14 11.9998C14 12.5521 14.4477 12.9998 15 12.9998L18.5859 12.9998L16.5858 14.9999L18 16.4141L21.7071 12.707C22.0976 12.3165 22.0976 11.6833 21.7071 11.2928L18 7.58569L16.5858 8.99991L18.5857 10.9998L15 10.9998ZM7.41418 9.00015L5.41409 11.0002L9 11.0002C9.55229 11.0002 10 11.448 10 12.0002C10 12.5525 9.55228 13.0002 9 13.0002L5.41428 13.0002L7.41418 15.0002L5.99997 16.4144L2.29286 12.7073C1.90234 12.3167 1.90234 11.6836 2.29286 11.293L5.99997 7.58594L7.41418 9.00015Z" fill="black" />
-                    </svg>
-                </div>
-            </div>
-            <div className={styles.bodyParent}>
-                <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}>
-                    <h2 className={styles.header}>Designing for Everything and Everyone</h2>
-                </div>
-                <p className={styles.body}>The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.</p>
-                <div className={styles.cardRow}>
-                    <div className={styles.card}></div>
-                    <div className={styles.card}></div>
-                    <div className={styles.card}></div>
-                </div>
-            </div>
+  return (
+    <div
+      id="responsive"
+      style={{
+        position: "relative",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <div id="handle" className={styles.handle}>
+        <div
+          style={{
+            display: "flex",
+            position: "absolute",
+            left: "-0.75rem",
+            width: "2rem",
+            height: "2rem",
+            borderRadius: "1rem",
+            backgroundColor: "#ccc",
+            alignItems: "center",
+          }}
+        >
+          <svg
+            style={{ margin: "auto" }}
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 4C12.5523 4 13 4.44772 13 5L13 19C13 19.5523 12.5523 20 12 20C11.4477 20 11 19.5523 11 19L11 5C11 4.44772 11.4477 4 12 4Z"
+              fill="black"
+              fillOpacity="0.4"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M15 10.9998C14.4477 10.9998 14 11.4475 14 11.9998C14 12.5521 14.4477 12.9998 15 12.9998L18.5859 12.9998L16.5858 14.9999L18 16.4141L21.7071 12.707C22.0976 12.3165 22.0976 11.6833 21.7071 11.2928L18 7.58569L16.5858 8.99991L18.5857 10.9998L15 10.9998ZM7.41418 9.00015L5.41409 11.0002L9 11.0002C9.55229 11.0002 10 11.448 10 12.0002C10 12.5525 9.55228 13.0002 9 13.0002L5.41428 13.0002L7.41418 15.0002L5.99997 16.4144L2.29286 12.7073C1.90234 12.3167 1.90234 11.6836 2.29286 11.293L5.99997 7.58594L7.41418 9.00015Z"
+              fill="black"
+            />
+          </svg>
         </div>
-    )
+      </div>
+      <div className={styles.bodyParent}>
+        <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}>
+          <h2 className={styles.header}>Designing for Everyone</h2>
+        </div>
+        <p className={styles.body}>
+          From massive monitors to minuscule mobiles, every good design should
+          be accessible to everyone. That's why accessibility and usability are
+          the keys to a perfect design and in turn, designer.
+        </p>
+        <div className={styles.cardRow}>
+          <div className={[styles.card, styles.mobile].join(" ")}>
+            <b>Mobile</b>
+            <div
+              style={{ position: "relative", height: "100%", width: "100%" }}
+            >
+              <Image
+                src="/mobile.png"
+                alt=""
+                layout="responsive"
+                width="100%"
+                height="100%"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className={[styles.card, styles.tablet].join(" ")}>
+            <b>Tablet</b>
+            <div
+              style={{ position: "relative", height: "100%", width: "100%" }}
+            >
+              <Image
+                src="/tablet.png"
+                alt=""
+                layout="responsive"
+                width="100%"
+                height="100%"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+          <div className={[styles.card, styles.desktop].join(" ")}>
+            <b>Desktop</b>
+            <div
+              style={{ position: "relative", height: "100%", width: "100%" }}
+            >
+              <Image
+                src="/desktop.png"
+                alt=""
+                layout="responsive"
+                width="100%"
+                height="100%"
+                objectFit="contain"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image';
-import rickroll from '/public/rickroll.gif';
-import Loader from '../components/Loader'
-import Counter from '../components/Landing/Counter';
-import Responsive from '../components/Landing/Responsive';
-import { useRef, useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import Loader from "../components/Loader";
+import Counter from "../components/Landing/Counter";
+import Responsive from "../components/Landing/Responsive";
 
-import { motion } from 'framer-motion';
-import { InView } from 'react-intersection-observer';
+import { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { InView } from "react-intersection-observer";
+import { TypeAnimation } from "react-type-animation";
 
-import styles from '../styles/index.module.scss'
-
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
   let from = 0;
   let to = 80;
 
-
   // prevent page from scrolling
-  if (typeof window !== 'undefined') {
+  if (typeof window !== "undefined") {
     if (loading) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = "auto";
     }
   }
 
@@ -40,28 +38,65 @@ export default function Home() {
         <div className={styles.hero}>
           <div className={styles.heroHeadings}>
             <h3 className={styles.subheading}>Meet a next generation</h3>
-            <h1 className={styles.heading}>Product Designer</h1>
+            <TypeAnimation
+              sequence={[
+                "Product Designer",
+                4000,
+                "UX Researcher",
+                4000,
+                "Copywriter",
+                4000,
+                "UI Designer",
+                4000,
+                "Frontend Engineer",
+                4000,
+              ]}
+              className={styles.heading}
+              wrapper="h2"
+              repeat={Infinity}
+            />
           </div>
           <iframe
             id="spline"
-            src='https://my.spline.design/portfoliohero-42c2c4614b0b42afdeb45b1d42f83c1f/'
-            width='100%'
-            height='100%'
-            frameBorder='0'
+            src="https://my.spline.design/portfoliohero-42c2c4614b0b42afdeb45b1d42f83c1f/"
+            width="100%"
+            height="100%"
+            frameBorder="0"
             onLoad={() => {
-              setLoading(false)
+              setLoading(false);
             }}
           ></iframe>
         </div>
         <section id="about" className={styles.about}>
           <div className={styles.tag}>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path opacity="0.4" d="M4 3C3.448 3 3 3.448 3 4V9C3 9.552 3.448 10 4 10C4.552 10 5 9.552 5 9V5H9C9.552 5 10 4.552 10 4C10 3.448 9.552 3 9 3H4ZM15 3C14.448 3 14 3.448 14 4C14 4.552 14.448 5 15 5H19V9C19 9.552 19.448 10 20 10C20.552 10 21 9.552 21 9V4C21 3.448 20.552 3 20 3H15ZM4 14C3.448 14 3 14.448 3 15V20C3 20.552 3.448 21 4 21H9C9.552 21 10 20.552 10 20C10 19.448 9.552 19 9 19H5V15C5 14.448 4.552 14 4 14ZM20 14C19.448 14 19 14.448 19 15V19H15C14.448 19 14 19.448 14 20C14 20.552 14.448 21 15 21H20C20.552 21 21 20.552 21 20V15C21 14.448 20.552 14 20 14Z" fill="#03CDFD" />
-              <path d="M12.0112 6C10.3555 6 9.01345 7.343 9.01345 9C9.01345 10.657 10.3555 12 12.0112 12C13.667 12 15.009 10.657 15.009 9C15.009 7.343 13.667 6 12.0112 6ZM11.012 13C9.28525 13 7.54055 14.118 7.04592 15.688C6.84307 16.332 7.33869 17 8.01419 17C9.02844 17 15.5026 17 16.0083 17C16.6928 17 17.1654 16.336 16.9456 15.688C16.4249 14.155 14.6972 13 13.0105 13H11.012Z" fill="#03CDFD" />
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                opacity="0.4"
+                d="M4 3C3.448 3 3 3.448 3 4V9C3 9.552 3.448 10 4 10C4.552 10 5 9.552 5 9V5H9C9.552 5 10 4.552 10 4C10 3.448 9.552 3 9 3H4ZM15 3C14.448 3 14 3.448 14 4C14 4.552 14.448 5 15 5H19V9C19 9.552 19.448 10 20 10C20.552 10 21 9.552 21 9V4C21 3.448 20.552 3 20 3H15ZM4 14C3.448 14 3 14.448 3 15V20C3 20.552 3.448 21 4 21H9C9.552 21 10 20.552 10 20C10 19.448 9.552 19 9 19H5V15C5 14.448 4.552 14 4 14ZM20 14C19.448 14 19 14.448 19 15V19H15C14.448 19 14 19.448 14 20C14 20.552 14.448 21 15 21H20C20.552 21 21 20.552 21 20V15C21 14.448 20.552 14 20 14Z"
+                fill="#03CDFD"
+              />
+              <path
+                d="M12.0112 6C10.3555 6 9.01345 7.343 9.01345 9C9.01345 10.657 10.3555 12 12.0112 12C13.667 12 15.009 10.657 15.009 9C15.009 7.343 13.667 6 12.0112 6ZM11.012 13C9.28525 13 7.54055 14.118 7.04592 15.688C6.84307 16.332 7.33869 17 8.01419 17C9.02844 17 15.5026 17 16.0083 17C16.6928 17 17.1654 16.336 16.9456 15.688C16.4249 14.155 14.6972 13 13.0105 13H11.012Z"
+                fill="#03CDFD"
+              />
             </svg>
             About Me
           </div>
-          <motion.div style={{ position: "relative", padding: "2rem 0", width: "100%", display: "flex", flexDirection: "column", gap: "1.25rem" }}
+          <motion.div
+            style={{
+              position: "relative",
+              padding: "2rem 0",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "1.25rem",
+            }}
             initial={{
               opacity: 0,
               y: 100,
@@ -72,8 +107,11 @@ export default function Home() {
             }}
             transition={{
               delay: 0.15,
-            }}>
-            <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}>
+            }}
+          >
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}
+            >
               <InView as="div" threshold={0.25}>
                 {({ inView, ref }) => {
                   return (
@@ -86,9 +124,21 @@ export default function Home() {
               <h2>Years of Updates</h2>
               <span className={styles.amen}>(AMEN)</span>
             </div>
-            <p>With an actual lifetime guarantee, you can expect non-stop updates to forever improve your Imo experience.</p>
+            <p>
+              Hi I'm Imo! With an actual lifetime guarantee, you can expect
+              non-stop updates to forever improve your Imo experience.
+            </p>
           </motion.div>
-          <motion.div style={{ position: "relative", padding: "2rem 0", width: "100%", display: "flex", flexDirection: "column", gap: "1.25rem", cursor: "grab" }}
+          <motion.div
+            style={{
+              position: "relative",
+              padding: "2rem 0",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "1.25rem",
+              cursor: "grab",
+            }}
             initial={{
               opacity: 0,
               y: 100,
@@ -109,13 +159,29 @@ export default function Home() {
             }}
             dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
             dragElastic={0.5}
-            whileTap={{ cursor: "grabbing" }}>
-            <div style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}>
+            whileTap={{ cursor: "grabbing" }}
+          >
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}
+            >
               <h2>A True Pixel Perfectionist</h2>
             </div>
-            <p>A single pixel can be the difference between a great design and a perfect one. That’s why high-quality products rely on people who care about the most minute of details and can ensure you with the highest possible standards of design.</p>
+            <p>
+              A single pixel can be the difference between a great design and a
+              perfect one. That’s why high-quality products rely on people who
+              care about the most minute of details and can ensure you with the
+              highest possible standards of design.
+            </p>
           </motion.div>
-          <motion.div id="responsiveParent" style={{ position: "relative", display: "flex", width: "100%", backgroundColor: "#00000010", borderRadius: "1.25rem", alignItems: "center" }}
+          <motion.div
+            id="responsiveParent"
+            style={{
+              position: "relative",
+              display: "flex",
+              width: "100%",
+              backgroundColor: "#00000010",
+              borderRadius: "1.25rem",
+            }}
             initial={{
               opacity: 0,
               y: 100,
@@ -126,7 +192,8 @@ export default function Home() {
             }}
             transition={{
               delay: 0.25,
-            }}>
+            }}
+          >
             {/* <motion.div id="handle" className={styles.handle}
             drag
             dragConstraints={{
@@ -134,15 +201,52 @@ export default function Home() {
 
             </motion.div> */}
 
-            <Image src={rickroll} alt="" layout='fill' objectFit='contain' />
+            <Image
+              src="/rickroll.gif"
+              alt=""
+              layout="fill"
+              objectFit="contain"
+            />
             <Responsive />
           </motion.div>
+          <motion.div
+            style={{
+              position: "relative",
+              padding: "2rem 0",
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              gap: "1.25rem",
+              cursor: "grab",
+            }}
+            initial={{
+              opacity: 0,
+              y: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              delay: 0.2,
+            }}
+          >
+            <div
+              style={{ display: "flex", flexDirection: "row", gap: "0.5rem" }}
+            >
+              <h2>Tested and Benchmarked</h2>
+            </div>
+            <p>
+              Sometimes, a fancy website isn't enough to fully illustrate the
+              capabilities of a designer. Sometimes, you need hard numbers. To
+              guarantee industry levels of quality, thorough and comprehensive
+              testing was conducted through numerous reputable benchmarks to
+              ensure top notch design.
+            </p>
+          </motion.div>
         </section>
-        <section>
-        </section>
+        <section></section>
       </main>
     </div>
   );
-
 }
-
